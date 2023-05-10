7c520b0734c9251db8243dcf9a2bd38d9e0494bb
Steven Cao <caohc2017@gmail.com>
2023-05-09 21:38:44 -0400

    Use preferred theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
-import { Container, Typography } from "@mui/material";
+import { Container, CssBaseline, ThemeProvider, Typography, createTheme, useMediaQuery } from "@mui/material";
 import Ruse from "./Ruse";
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 
 export default function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+
+  const theme = useMemo(() => createTheme({
+    palette: {
+      mode: prefersDarkMode ? "dark" : "light"
+    }
+  }), [prefersDarkMode]);
+
   useEffect(() => {
     document.title = "Ruse Intepreter";
   })
 
   return (
-    <Container maxWidth="sm" sx={{my: 4}}>
-      <Typography variant="h4" sx={{my: 2}}>
-        Ruse Interpreter
-      </Typography>
-      <Ruse />
-    </Container>
+    <ThemeProvider theme={theme}>
+      <CssBaseline/>
+      <Container maxWidth="sm" sx={{my: 4}}>
+        <Typography variant="h4" sx={{my: 2}}>
+          Ruse Interpreter
+        </Typography>
+        <Ruse />
+      </Container>
+    </ThemeProvider>
+    
   );
 }
